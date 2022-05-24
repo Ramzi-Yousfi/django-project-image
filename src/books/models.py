2ccce3060161cf448cb07836fc002733b57e0ffb
ramzi <ramzi446@hotmail.com>
2022-05-24 12:59:35 +0200

    first model

--- src/books/models.py
+++ src/books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Books(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.CharField(max_length=150)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    pageCount = models.IntegerField()
+    published_date = models.DateField(default=datetime.now)
+    thumbnail = models.CharField(max_length=256)
+        
+    
